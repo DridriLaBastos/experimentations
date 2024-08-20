68e1b2188fab113aacbfd60ec3b2e16aac0f422d
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-08-20 22:09:02 +0200

    [WUST] Tcp connection

--- wust/src/main.rs
+++ wust/src/main.rs
@@ -1,3 +1,22 @@
+use std::net::TcpListener;
+
+fn server_loop(listener: TcpListener)
+{
+	for stream in listener.incoming()
+	{
+		match stream
+		{
+			Ok(s) => println!("Connection established {s:?}"),
+			Err(e) => panic!("Unable to create a connection : {e:?}"),
+		}
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	let did_listen = TcpListener::bind("127.0.0.1:80");
+
+	match did_listen {
+		Ok(listener) => server_loop(listener),
+		Err(e) => panic!("Unable to bind server to port 7878 : error {e:?}"),
+	}
 }
