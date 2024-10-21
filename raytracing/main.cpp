0257d8b3b06b2ddffb7d28014503f07ff607f905
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-10-21 21:50:53 +0200

    [RT] Using SFML to create a window

--- raytracing/main.cpp
+++ raytracing/main.cpp
@@ -5,15 +5,19 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include "SFML/Graphics.hpp"
+
 #include "ppm.hpp"
 #include "color.hpp"
+
+#include "util/module/module.hpp"
 
 constexpr size_t IMAGE_WIDTH = 720;
 constexpr size_t IMAGE_HEIGHT = 480;
 
-
 int main(void)
 {
+#if 0
 	PPMUtil ppm (IMAGE_WIDTH,IMAGE_HEIGHT);
 
 	for (size_t y = 0; y < ppm.height; y += 1)
@@ -27,6 +31,30 @@ int main(void)
 	}
 
 	ppm.write("test.ppm",12);
+#endif
+
+	sf::RenderWindow window (sf::VideoMode(640,480),"Raytracing");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+				
+				default:
+					break;
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 
 	return EXIT_SUCCESS;
 }
