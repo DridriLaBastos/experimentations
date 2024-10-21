e60b81820164eedec2fe668d64e0430319da7cbf
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-10-21 22:59:00 +0200

    [MODULE] Bug correction : * Loading the copied shared lib * Updating the read time variable

--- util/module/src/module.cpp
+++ util/module/src/module.cpp
@@ -7,11 +7,11 @@ Module::Module(const std::filesystem::path& modulePath,const std::filesystem::pa
 {
 	if (std::filesystem::exists(modulePath))
 	{
-		if (!Platform::LoadSharedModule(modulePath,&mSharedModule))
+		const std::error_code errorCode = CopyAndLoad();
+		if (errorCode)
 		{
 			fprintf(stderr,"Loading '%s' failed with error %d : '%s'\n",modulePath.c_str(),Platform::GetSharedModuleErrorCode(),Platform::GetSharedModuleErrorDescription(Platform::GetSharedModuleErrorCode()).c_str());
 		}
-		mModuleLastWriteTime = std::filesystem::last_write_time(modulePath);
 	}
 }
 
@@ -21,6 +21,22 @@ Module::~Module()
 	{
 		fprintf(stderr, "Unloading module '%s' failed with error %d : '%s'",mModulePath.c_str(),Platform::GetSharedModuleErrorCode(),Platform::GetSharedModuleErrorDescription(Platform::GetSharedModuleErrorCode()).c_str());
 	}
+}
+
+std::error_code Module::CopyAndLoad()
+{
+	std::error_code errorCode;
+	const bool status = std::filesystem::copy_file(mModulePath,mModuleCopyPath,std::filesystem::copy_options::overwrite_existing,errorCode);
+
+	if (!status)
+	{
+		return errorCode;
+	}
+
+	mModuleLastWriteTime = std::filesystem::last_write_time(mModuleCopyPath);
+	Platform::UnloadSharedModule(&mSharedModule);
+	Platform::LoadSharedModule(mModuleCopyPath,&mSharedModule);
+	return errorCode;
 }
 
 bool Module::NeedReload() const
@@ -40,15 +56,12 @@ bool Module::ReloadIfNeeded()
 		return false;
 	}
 
-	std::error_code errorCode;
-	const bool status = std::filesystem::copy_file(mModulePath,mModuleCopyPath,std::filesystem::copy_options::overwrite_existing,errorCode);
-
-	if (!status)
+	const std::error_code errorCode = CopyAndLoad();
+	if (errorCode)
 	{
 		fprintf(stderr, "Copying '%s' failed with error '%d' : '%s'",mModulePath.c_str(),errorCode.value(),Platform::GetPlatformErrorCodeDescription(errorCode.value()).c_str());
 		return false;
 	}
 
-	Platform::UnloadSharedModule(&mSharedModule);
-	return Platform::LoadSharedModule(mModuleCopyPath,&mSharedModule);
+	return true;
 }
