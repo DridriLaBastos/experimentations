fe55faf402b1b31179b88efcf42d0d72d0139735
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-10-21 23:00:10 +0200

    [MODULE] Utility defines for unic constructor and destructor of shared lib

--- util/module/platform_util.hpp
+++ util/module/platform_util.hpp
@@ -7,4 +7,16 @@
 #define DLL_EXPORT __attribute__((visibility("default")))
 #endif
 
+#ifdef WIN32
+#warning "DLL_INIT not defined for WIN32 platform"
+#else
+#define DLL_INIT __attribute__((constructor))
+#endif
+
+#ifdef WIN32
+#warning "DLL_CLEAR not defined for WIN32 platform"
+#else
+#define DLL_CLEAR __attribute__((destructor))
+#endif
+
 #endif
