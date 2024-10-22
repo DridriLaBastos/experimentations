e7116a1fa2d5cb29f344c6d93e3502ed578c2090
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-10-22 10:30:40 +0200

    [MODULE] wwin32 platform Create platform_win32.cpp

---
+++ util/module/src/platform_win32.cpp
@@ -0,0 +1,42 @@
+#include "platforme.hpp"
+
+SharedModuleError Platform::GetSharedModuleErrorCode()
+{
+	return GetLastError();
+}
+
+std::string Platform::GetSharedModuleErrorDescription(const SharedModuleError errorCode)
+{
+	return GetPlatformErrorCodeDescription((PlatformErrorCode)errorCode);
+}
+
+std::string Platform::GetPlatformErrorCodeDescription(const PlatformErrorCode error)
+{
+	LPSTR msgBuffer = NULL;
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,NULL,error,MAKELANGID(LANG_NEUTRAL,SUBLANG_DEFAULT),msgBuffer,0,NULL);
+
+	std::string ret (msgBuffer);
+
+	LocalFree(msgBuffer);
+
+	return ret;
+}
+
+bool Platform::LoadSharedModule(const std::filesystem::path path, SharedModuleHandle* outHandle)
+{
+	HMODULE module = LoadLibrary(path.generic_string().c_str());
+	*outHandle = module;
+	return module != NULL;
+}
+
+bool Platform::UnloadSharedModule(SharedModuleHandle* sharedModuleHandle)
+{
+	return FreeLibrary(*sharedModuleHandle);
+}
+
+bool LoadSymbol(const SharedModuleHandle* sharedModuleHandle, const std::string& symbol, void** sym)
+{
+	FARPROC proc = GetProcAddress(*sharedModuleHandle,symbol.c_str());
+	*sym = (void*)proc;
+	return proc != NULL;
+}
