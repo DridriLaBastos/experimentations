aaa013682b856ea445774c0ba863adbd8ada47c8
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-10-22 22:27:17 +0200

    [RT] Book first step

--- raytracing/raytracing.cpp
+++ raytracing/raytracing.cpp
@@ -1,16 +1,24 @@
+#include <cstdio>
 #include <unistd.h>
 
 #include "raytracing.hpp"
 
 RAYTRACING_DRAW_MODULE_FUNC_DEFINITION
 {
-	for (size_t i = 0; i < (renderingInfo->height)*(renderingInfo->width); i += 1)
+	for (size_t y = 0; y < renderingInfo->height; y += 1)
 	{
-		renderingInfo->buffer[i] = Colors::MAGENTA;
+		for (size_t x = 0; x < renderingInfo->width; x += 1)
+		{
+			const float r = x / ((float)renderingInfo->width - 1);
+			const float g = y / ((float)renderingInfo->height - 1);
+			const float b = 0;
+
+			const size_t pixelBufferIndex = y * renderingInfo->width + x;
+			renderingInfo->buffer[pixelBufferIndex] = Color(r,g,b);
+		}
 	}
 }
 
-#include <cstdio>
 
 DLL_INIT void Inti(void)
 {
