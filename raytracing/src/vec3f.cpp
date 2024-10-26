fd9105c8e2f03f1a01cdfb0f9f8ed2c9c2eae2b3
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-10-26 19:18:53 +0200

    [RT] Adding vector operations

--- raytracing/src/vec3f.cpp
+++ raytracing/src/vec3f.cpp
@@ -1,10 +1,34 @@
+#include <cmath>
+
 #include "vec3f.hpp"
 
-Vec3f& Vec3f::operator*= (const float x)
+Vec3f::Vec3f(const float _x, const float _y, const float _z): x(_x),y(_y),z(_z)
+{}
+
+float Vec3f::Length(void) const
 {
-	this->x *= x;
-	this->y *= y;
-	this->z *= z;
+	return std::sqrtf(x*x + y*y + z*z);
+}
+
+Vec3f Vec3f::Normalize(const Vec3f& v)
+{
+	return v / v.Length();
+}
+
+Vec3f& Vec3f::operator*= (const float _x)
+{
+	this->x *= _x;
+	this->y *= _x;
+	this->z *= _x;
+
+	return *this;
+}
+
+Vec3f& Vec3f::operator/=(const float _x)
+{
+	this->x /= _x;
+	this->y /= _x;
+	this->z /= _x;
 
 	return *this;
 }
@@ -33,6 +57,13 @@ Vec3f operator* (const float x,const Vec3f& v)
 {
 	Vec3f ret (v);
 	ret *= x;
+	return ret;
+}
+
+Vec3f operator/ (const Vec3f& v, const float x)
+{
+	Vec3f ret (v);
+	ret /= x;
 	return ret;
 }
 
