7c387ab05e277d920544fccd6b7330714b819966
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-11-15 15:14:57 +0000

    [NN] Sigmoid activation function

--- nn/main.cpp
+++ nn/main.cpp
@@ -4,15 +4,26 @@
 
 #include <random>
 #include <cstdio>
+#include <cmath>
 
 static float trainingSet [][3] = {
-	{0,0,1},
-	{0,1,2},
-	{1,0,2},
-	{1,1,2},
+	{0,0,0},
+	{0,1,1},
+	{1,0,1},
+	{1,1,1},
 };
 
-static float Cost (const float w0, const float w1, const float w2, bool verbose = false)
+static float Sigmoid (const float x)
+{
+	return 1.f / (1.f + std::exp(-x));
+}
+
+static float Evaluate (const float x0, const float x1, const float w0, const float w1, const float b)
+{
+	return Sigmoid(w0*x0+w1*x1+b);
+}
+
+static float Cost (const float w0, const float w1, const float b, bool verbose = false)
 {
 	float finalError = 0;
 
@@ -22,7 +33,7 @@ static float Cost (const float w0, const float w1, const float w2, bool verbose 
 
 		const float x0 = trainingSet[i][0];
 		const float x1 = trainingSet[i][1];
-		const float guess = w0*x0 + w1*x1 + w2;
+		const float guess = Evaluate(x0,x1,w0,w1,b);
 		const float error = expected - guess;
 		finalError += error*error;
 
@@ -35,19 +46,19 @@ static float Cost (const float w0, const float w1, const float w2, bool verbose 
 	return finalError / std::size(trainingSet);
 }
 
-static float DeriveW0(const float h, const float w0, const float w1, const float w2)
+static float DeriveW0(const float h, const float w0, const float w1, const float b)
 {
-	return (Cost(w0+h,w1,w2) - Cost(w0,w1,w2))/h;
+	return (Cost(w0+h,w1,b) - Cost(w0,w1,b))/h;
 }
 
-static float DeriveW1(const float h, const float w0, const float w1, const float w2)
+static float DeriveW1(const float h, const float w0, const float w1, const float b)
 {
-	return (Cost(w0,w1+h,w2) - Cost(w0,w1,w2))/h;
+	return (Cost(w0,w1+h,b) - Cost(w0,w1,b))/h;
 }
 
-static float DeriveW2(const float h, const float w0, const float w1, const float w2)
+static float DeriveB(const float h, const float w0, const float w1, const float b)
 {
-	return (Cost(w0,w1,w2+h) - Cost(w0,w1,w2))/h;
+	return (Cost(w0,w1,b+h) - Cost(w0,w1,b))/h;
 }
 
 int main(void)
@@ -55,26 +66,26 @@ int main(void)
 	// Seed with a real random value if available
 	std::random_device r;
 	std::mt19937 e (r());
-	std::uniform_real_distribution<float> d (10,100);
+	std::uniform_real_distribution<float> d (0,2);
 
-	float w0 = d(e), w1 = d(e), w2 = d(e);
-	const float eps = 2e-3;
+	float w0 = d(e), w1 = d(e), b = d(e);
+	const float eps = 1e-2;
 
 	//Apllying a rate because the value of the derivative might be too big
-	const float rate = 1e-3;
+	const float rate = 1e-1;
 
 	for (size_t i = 0; i < 10000; i += 1)
 	{
-		const float dW0 = DeriveW0(eps,w0,w1,w2) * rate;
-		const float dW1 = DeriveW1(eps,w0,w1,w2) * rate;
-		const float dW2 = DeriveW2(eps,w0,w1,w2) * rate;
+		const float dW0 = DeriveW0(eps,w0,w1,b) * rate;
+		const float dW1 = DeriveW1(eps,w0,w1,b) * rate;
+		const float db = DeriveB(eps,w0,w1,b) * rate;
 		w0 -= dW0;
 		w1 -= dW1;
-		w2 -= dW2;
-		printf("Final error : %.3f\n", Cost(w0,w1,w2));
+		b -= db;
+		printf("Final error : %.3f\n", Cost(w0,w1,b));
 	}
 
-	Cost(w0,w1,w2,true);
+	Cost(w0,w1,b,true);
 
 	return EXIT_SUCCESS;
 }
