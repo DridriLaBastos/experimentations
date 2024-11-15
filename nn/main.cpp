df9c19c11e9c905a0beeb1302d1a53367a583516
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-11-15 14:06:29 +0000

    [NN] C++ Random Number generator

--- nn/main.cpp
+++ nn/main.cpp
@@ -2,30 +2,16 @@
 // Created by Adrien COURNAND on 03/08/2024.
 //
 
+#include <random>
 #include <iostream>
-
-float trainSet [4][2] = {
-		{1},{2},{3},{4},
-};
-
-template <size_t _M, size_t _N, typename _T>
-static void Fill (const double a, const double b, _T in_out [_M][_N])
-{
-	for (size_t i = 0; i < _M; i += 1)
-	{
-		in_out[i][1] = a * in_out[i][0] + b;
-	}
-}
 
 int main(void)
 {
-	Fill<4,2>(2,0,trainSet);
-
-	for (auto trainEntry: trainSet)
-	{
-		printf("TrainSer %.2f -> %.2f\n",trainEntry[0],trainEntry[1]);
-	}
-
-	std::cout << "Hello neural network world\n";
+	// Seed with a real random value if available
+	std::random_device r;
+	std::mt19937 e (r());
+	std::uniform_real_distribution<float> d;
+	std::cout << d(e) << std::endl;
 	return EXIT_SUCCESS;
 }
+
