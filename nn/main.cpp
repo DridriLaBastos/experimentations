4045cb025c2ccc8ec348c58ace4a0c4cdf685bf1
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-11-15 14:21:18 +0000

    [NN] Adding a cost function

--- nn/main.cpp
+++ nn/main.cpp
@@ -3,15 +3,47 @@
 //
 
 #include <random>
-#include <iostream>
+#include <cstdio>
+
+static float trainingSet [][2] = {
+	{0,1},
+	{1,3},
+	{2,5},
+	{3,7},
+	{4,9},
+};
+
+static float Cost (const float w0, const float w1, const bool verbose = false)
+{
+	float finalError = 0;
+
+	for (size_t i = 0; i < std::size(trainingSet);i += 1)
+	{
+		const float expected = trainingSet[i][1];
+
+		const float x = trainingSet[i][0];
+		const float guess = w1*x + w0;
+		const float error = expected - guess;
+		finalError += error*error;
+
+		if(verbose)
+		{
+			printf("%.3f => %.3f (%.3f) e: %.3f (%.3f)\n",x,guess, expected,error,finalError);
+		}
+	}
+
+	return finalError / std::size(trainingSet);
+}
 
 int main(void)
 {
 	// Seed with a real random value if available
 	std::random_device r;
 	std::mt19937 e (r());
-	std::uniform_real_distribution<float> d;
-	std::cout << d(e) << std::endl;
+	std::uniform_real_distribution<float> d (10,100);
+
+	float w0 = d(e), w1 = d(e);
+	printf("Final error : %.3f\n", Cost(w0,w1));
+
 	return EXIT_SUCCESS;
 }
-
