763f4de0e91ba0036157f9b6fcb37ddf501d0316
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-12-18 21:10:33 +0000

    [NN] Move to a matrice framework

--- nn/nn.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef NN_HEADER_ONLY
-#include "nn.hpp"
-#endif
-
-Layer::Layer()
-{
-    
-}
--- nn/nn.hpp
+++ nn/nn.hpp
@@ -1,15 +1,41 @@
 #ifndef NN_HPP
 #define NN_HPP
 
-class Layer
+#include <memory>
+#include <cstdlib>
+
+template <typename _T>
+struct MallocDeleter {
+    void operator()(_T* ptr)
+    {
+        free(ptr);
+        ptr = nullptr;
+    }
+};
+
+template<typename _T>
+_T* MallocHelper(const size_t arrayCount = 1)
+{
+    return (_T*)malloc(sizeof(_T)*arrayCount);
+}
+
+template <typename _T>
+using MallocDeleterUniquePtr = std::unique_ptr<_T,MallocDeleter<_T> >;
+
+template <unsigned int Row, unsigned int Column>
+class Matrice
 {
     public:
-        Layer(void);
+        Matrice(void);
+
+        void operator+= (const Matrice& m);
+    
     private:
+        MallocDeleterUniquePtr<float> mWeights;
 };
 
 #if NN_HEADER_ONLY
-#include "nn.cpp"
+#include "matrice.cpp"
 #endif
 
 #endif
