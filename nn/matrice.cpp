27ab800d5a40c7854da83a8856c08bd1baff5ba9
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-12-18 21:10:33 +0000

    [NN] Move to a matrice framework

---
+++ nn/matrice.cpp
@@ -0,0 +1,19 @@
+#ifndef NN_HEADER_ONLY
+#include "nn.hpp"
+#endif
+
+template <unsigned int Row, unsigned int Column>
+Matrice<Row,Column>::Matrice(): mWeights(MallocHelper<float>(Row*Column),MallocDeleter<float>())
+{
+}
+
+template <unsigned int Row, unsigned int Column>
+void Matrice<Row,Column>::operator+=(const Matrice<Row,Column>& m)
+{
+    const size_t size = Row*Column;
+
+    for (size_t i = 0; i < size; i += 1)
+    {
+        mWeights.get()[i] += m.mWeights.get()[i];
+    }
+}
