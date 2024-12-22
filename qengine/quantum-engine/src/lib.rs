efcc53a3700f35b7522ae73e23b0709f40249000
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2024-12-22 23:36:45 +0100

    [QENGINE] Data communication between js and wasm

--- qengine/quantum-engine/src/lib.rs
+++ qengine/quantum-engine/src/lib.rs
@@ -1,6 +1,9 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-	a + b
+pub fn simulate(buffer: &mut [f32], width: u32, height: u32) 
+{
+	for i in 0..((width*height) as usize) {
+		buffer[i] = i as f32 / ((width*height) as f32);
+	}
 }
--- qengine/script.js
+++ qengine/script.js
@@ -1,18 +1,36 @@
-import init , { add } from './quantum-engine/pkg/quantum_engine.js';
+import init , { simulate } from './quantum-engine/pkg/quantum_engine.js';
 
 const ASPECT_RATIO = 16/9;
-const SIM_WIDTH = 400;
-const SIM_HEIGHT = SIM_WIDTH / ASPECT_RATIO;
+const DISP_WIDTH = 400;
+const DISP_HEIGHT = DISP_WIDTH / ASPECT_RATIO;
+
+const SIM_RATIO = 3;
+
+const SIM_WIDTH = DISP_WIDTH / SIM_RATIO;
+const SIM_HEIGHT = DISP_HEIGHT / SIM_RATIO;
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const simulation = new Float32Array(SIM_WIDTH * SIM_HEIGHT);
 
-canvas.setAttribute('width', SIM_WIDTH);
-canvas.setAttribute('height', SIM_HEIGHT);
+canvas.setAttribute('width', DISP_WIDTH);
+canvas.setAttribute('height', DISP_HEIGHT);
 canvas.style.borderRadius = '8px';
 canvas.style.border = '2px solid black';
 
 init().then(() => {
-	console.log("Wasm module loaded");
-	console.log(add(1, 2));
+	simulate(simulation, SIM_WIDTH, SIM_HEIGHT);
+
+	const imageData = ctx.createImageData(SIM_WIDTH, SIM_HEIGHT);
+
+	for (let i = 0; i < simulation.length; i++) {
+		const pixel = simulation[i] * 255;
+		imageData.data[i * 4] = pixel;
+		imageData.data[i * 4 + 1] = pixel;
+		imageData.data[i * 4 + 2] = pixel;
+		imageData.data[i * 4 + 3] = 255;
+	}
+
+	ctx.putImageData(imageData, 0, 0);
+	ctx.drawImage(canvas, 0, 0, SIM_WIDTH, SIM_HEIGHT, 0, 0, DISP_WIDTH, DISP_HEIGHT);
 });
