3363ae36fed1721d055b08d4d78827eb2b0421e9
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2025-01-27 23:33:35 +0100

    [EVOLVE] Dynamic update of the texture

--- evolve/main.cpp
+++ evolve/main.cpp
@@ -8,34 +8,76 @@ constexpr unsigned int WORLD_CENTER_X = WINDOW_WIDTH / 2;
 constexpr unsigned int WORLD_CENTER_Y = WINDOW_HEIGHT / 4;
 constexpr float PIXEL_PRE_METER = 5.f;
 
+using Clock = std::chrono::high_resolution_clock;
+
+union Color {
+	uint32_t uival;
+	struct {
+		uint32_t alpha:8, blue:8, green:8, red:8;
+	};
+
+	constexpr operator uint32_t() const { return uival; }
+};
+
+struct Colors {
+	static constexpr Color White = {0xFFFFFFFF};
+	static constexpr Color Black = {0x000000FF};
+	static constexpr Color Red = {0xFF0000FF};
+	static constexpr Color Green = {0xFF00FF00};
+	static constexpr Color Blue = {0xFFFF0000};
+	static constexpr Color Yellow = {Red | Green};
+	static constexpr Color Magenta = {Red | Blue};
+	static constexpr Color Cyan = {Green | Blue};
+	static constexpr Color Gray = {0xFF808080};
+	static constexpr Color DarkGray = {0xFF404040};
+	static constexpr Color LightGray = {0xFFC0C0C0};
+
+};
+
+struct RenderData {
+	unsigned int height;
+	unsigned int width;
+	std::unique_ptr<Color> buffer;
+
+	RenderData(const unsigned int w, const unsigned int h) : height(h), width(w), buffer(new Color[w*h]) {}
+};
+
+static void PutRectangle(RenderData& renderData, const float x, const float y, const float w, const float h, const Color color)
+{
+	const int x0 = std::max(0, (int)x);
+	const int x1 = std::min((int)(x + w), (int)renderData.width);
+	const int y0 = std::max(0, (int)y);
+	const int y1 = std::min((int)(y + h), (int)renderData.height);
+
+	for (int j = y0; j < y1; j++)
+	{
+		for (int i = x0; i < x1; i++)
+		{
+			renderData.buffer.get()[(j * renderData.width) + i] = color;
+		}
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	b2Version version = b2GetVersion();
-	printf("Box2D version %d.%d.%d\n", version.major, version.minor, version.revision);
-	
-	b2WorldDef worlDef = b2DefaultWorldDef();
-	worlDef.gravity = (b2Vec2){0.0f, -9.8f};
-
-	b2WorldId worldId = b2CreateWorld(&worlDef);
-
-	b2BodyDef bodyDef = b2DefaultBodyDef();
-	bodyDef.position = (b2Vec2){0.0, 10};
-	bodyDef.type = b2BodyType::b2_dynamicBody;
-	b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
-
-	b2Polygon polygon = b2MakeRoundedBox(0.3,0.3,0.3);
-
-	b2ShapeDef shapeDef = b2DefaultShapeDef();
-	shapeDef.density = 1.0f;
-	shapeDef.friction = .8f;
-	b2CreatePolygonShape(bodyId, &shapeDef, &polygon);
+	RenderData renderData(720,480);
 
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Evolve");
 	window.setFramerateLimit(30);
 
-	sf::CircleShape circle (3.0*PIXEL_PRE_METER,16);
-	circle.setPosition(40, 40);
-	circle.setFillColor(sf::Color::Red);
+	sf::Texture texture;
+	texture.create(renderData.width, renderData.height);
+
+	sf::Sprite sprite(texture);
+
+	sf::Clock clock;
+
+	// PutRectangle(renderData, 0, 100, 100, 100, Colors::Green);
+	// PutRectangle(renderData, 0, 200, 100, 100, Colors::Blue);
+
+	Color color = Colors::Black;
+
+	PutRectangle(renderData, 0, 0, renderData.width, renderData.height, Colors::LightGray);
 
 	while (window.isOpen())
 	{
@@ -45,19 +87,19 @@ int main(int argc, char const *argv[])
 			if (event.type == sf::Event::Closed) window.close();
 		}
 
+		const float elapsed = clock.restart().asSeconds();
+
+		color.red += 50 * elapsed;
+		color.green += 100 * elapsed;
+		color.blue += 150 * elapsed;
+
+		PutRectangle(renderData, -10, -20, 100,100, color);
+		texture.update((sf::Uint8*)renderData.buffer.get());
+
 		window.clear();
-
-		b2Vec2 position = b2Body_GetPosition(bodyId);
-		b2World_Step(worldId, 1.0f/30.0f, 4);
-
-		circle.setPosition(WORLD_CENTER_X + position.x * PIXEL_PRE_METER, WORLD_CENTER_Y - position.y * PIXEL_PRE_METER);
-
-		window.draw(circle);
-
+		window.draw(sprite);
 		window.display();
 	}
-
-	b2DestroyWorld(worldId);
 
 	return 0;
 }
