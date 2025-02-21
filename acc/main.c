3e88dc7b12d2dd1e9e33f6a89913f7c73ed5b37b
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2025-02-21 23:02:01 +0100

    [ACC] Defining token

--- acc/main.c
+++ acc/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum E_TOKEN_TYPE
+{
+	TOKEN_TYPE_PLUS, TOKEN_TYPE_MINUS, TOKEN_TYPE_STAR, TOKEN_TYPE_SLASH, TOKEN_TYPE_INTLIST
+} TOKEN_TYPE;
+
+struct Token
+{
+	TOKEN_TYPE type;
+	int intValue;
+};
+
 int main(int argc, char const *argv[])
 {
 	printf("Hello, World!\n");
