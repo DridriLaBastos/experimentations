a3f9b9785ef242b9b5454a6da13a5338a7645be0
Adrien COURNAND <adrien.cournand.ext@safrangroup.com>
2025-03-25 09:58:39 +0100

    Adding document in the database

--- writing/models.py
+++ writing/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Document(models.Model):
+    creation = models.DateTimeField("creation_date")
+    modif    = models.DateTimeField("last_modified")
+    data     = models.TextField("markedown_content")
