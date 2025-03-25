8dcd8a04ece50e495ac7f1720816a6156779af2e
Adrien COURNAND <adrien.cournand.ext@safrangroup.com>
2025-03-25 10:25:35 +0100

    Serving html file

--- writing/views.py
+++ writing/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return HttpResponse("Good luck writing your duck")
 
 def edition(request):
-    return HttpResponse("Good luck editing your duck")
+    template = loader.get_template("writing/edition.html")
+    return render(request,"writing/edition.html")
