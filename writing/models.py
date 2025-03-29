1a07b51de95290c026acf119584318d00326248b
DridriLaBastos <19534959+DridriLaBastos@users.noreply.github.com>
2025-03-29 11:06:17 +0100

    Adding a name to the document

--- writing/models.py
+++ writing/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Document(models.Model):
+    name     = models.CharField("document_name",max_length=256)
     creation = models.DateTimeField("creation_date")
     modif    = models.DateTimeField("last_modified")
     data     = models.TextField("markedown_content")
