9b15f8b0d85a0bbb225462eb7aece58de37b8683
DridriLaBastos <19534959+DridriLaBastos@users.noreply.github.com>
2025-06-04 17:45:44 +0200

    [CONNECTION] Connection page send data connection to in a POST request

--- connection/urls.py
+++ connection/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.connect)
+    path("", views.connect),
+    path("connect", views.try_connect, name="connect"),
 ]
--- connection/views.py
+++ connection/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def connect(request):
     return render(request, "connection/connect.html")
+
+def try_connect(request):
+    print(f"REACHED: {request}")
+    return HttpResponse()
