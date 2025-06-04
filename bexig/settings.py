644f63e27baeb957acfc4d8da38734eb2d9076fe
DridriLaBastos <19534959+DridriLaBastos@users.noreply.github.com>
2025-06-04 16:21:04 +0200

    [CONNECT] Creating a connection page

--- bexig/settings.py
+++ bexig/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'writing.apps.WritingConfig',
+    'connection.apps.ConnectionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- bexig/urls.py
+++ bexig/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("writing.urls")),
+    path("", include("connection.urls")),
     path("admin/", admin.site.urls),
 ]
