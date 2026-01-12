b6f4c193c4188c584b4120edc506c453a369f771
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2026-01-12 23:27:45 +0100

    [GOGOLE] Don't need all that craps

--- qengine/quantum-engine/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn simulate(buffer: &mut [f32], width: u32, height: u32) 
-{
-	for i in 0..((width*height) as usize) {
-		buffer[i] = i as f32 / ((width*height) as f32);
-	}
-}
--- raytracing/src/hittable.cpp
+++
@@ -1,7 +0,0 @@
-#include "geometry/hittable.hpp"
-
-void HitRecord::SetFaceNormal(const Ray& r, const Vec3f& outwardNormal)
-{
-	frontFace = glm::dot(r.Direction(),outwardNormal) < 0;
-	normal = frontFace ? outwardNormal : -outwardNormal;
-}
