113d6fafc5135797bfdd2d3ea52fabff2b545fe6
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2026-08-10 22:48:27 +0200

    [EVOLVE] Fresh restart

--- evolve/src/SceneEntity.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Adrien COURNAND on 04/02/2025.
-//
-#include "scene/SceneEntity.hpp"
-
-SceneEntity::SceneEntity(entt::registry &registry): mRegistry(registry)
-{
-	mEntityId = mRegistry.create();
-}
